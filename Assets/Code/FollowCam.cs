5860ce8d05c8ebdd16450805d130f577f928a894
Jake Whitmer <jpwhit13@gmail.com>
2020-08-26 10:45:24 -0500

    Built level follow cam player controller

---
+++ Assets/Code/FollowCam.cs
@@ -0,0 +1,29 @@
+﻿//Jake Whimter
+// follow players x but not anything else
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowCam : MonoBehaviour
+{
+    [Tooltip("The object you want to follow")]
+    public Transform target;
+
+    //strarting pos of camera
+    Vector3 startPos;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        startPos = this.transform.position;
+    }
+
+    // Update is called once per frame
+    void LateUpdate()
+    {
+        // get pos of target but keep starting y and z
+        Vector3 newPos = new Vector3(target.position.x, startPos.y, startPos.z);
+        this.transform.position = newPos;
+    }
+}
---
+++ Assets/Code/PlayerController.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody))]
+public class PlayerController : MonoBehaviour
+{
+    Rigidbody rb;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = this.GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Debug.Log("Time since last frame = " + Time.deltaTime);
+
+        if(Input.anyKeyDown) Jump();
+    }
+    // Fixed update is for physics it runs 50 times per second.
+    void FixedUpdate()
+    {
+        Debug.Log("Fixed update time = " + Time.deltaTime);
+
+        //adding forward force
+        rb.AddRelativeForce(Vector3.right * 10);
+    }
+
+    void Jump(){
+        rb.AddRelativeForce(Vector3.up * 20, ForceMode.Impulse);
+    }
+}
