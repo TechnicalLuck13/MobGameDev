b5483f03fa2c397d972a3177aece5cb33190404f
Jake Whitmer <jpwhit13@gmail.com>
2020-08-26 11:02:52 -0500

    Speeds

--- Assets/Code/PlayerController.cs
+++ Assets/Code/PlayerController.cs
@@ -5,7 +5,15 @@ using UnityEngine;
 [RequireComponent(typeof(Rigidbody))]
 public class PlayerController : MonoBehaviour
 {
+    [Range(5,50)]
+    public float jumpSpeed = 20f;
+    [Range(5,50)]
+    public float fallSpeed = 20f;
+    [Range(5,50)]
+    public float forwardSpeed = 10f;
+
     Rigidbody rb;
+    bool isGrounded = false;
 
     // Start is called before the first frame update
     void Start()
@@ -19,6 +27,7 @@ public class PlayerController : MonoBehaviour
         //Debug.Log("Time since last frame = " + Time.deltaTime);
 
         if(Input.anyKeyDown) Jump();
+        
     }
     // Fixed update is for physics it runs 50 times per second.
     void FixedUpdate()
@@ -26,10 +35,22 @@ public class PlayerController : MonoBehaviour
         Debug.Log("Fixed update time = " + Time.deltaTime);
 
         //adding forward force
-        rb.AddRelativeForce(Vector3.right * 10);
+        rb.AddRelativeForce(Vector3.right * forwardSpeed);
+
+        if(isGrounded == false){
+            rb.AddRelativeForce(Vector3.down * fallSpeed);
+        }
     }
 
     void Jump(){
-        rb.AddRelativeForce(Vector3.up * 20, ForceMode.Impulse);
+        if(isGrounded){
+            rb.AddRelativeForce(Vector3.up * jumpSpeed, ForceMode.Impulse);
+            isGrounded = false;
+        }
+    }
+    void OnTriggerEnter(Collider other){
+        if(other.gameObject.CompareTag("Ground")){
+            isGrounded = true;
+        }
     }
 }
